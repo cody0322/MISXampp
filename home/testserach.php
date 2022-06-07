13d710ed3dd07bbc87a7c11455b81f6f393e168e
cody0322 <z1510110@gmail.com>
2022-06-08 00:52:10 +0800

    OK

--- home/testserach.php
+++ home/testserach.php
@@ -15,31 +15,33 @@ $school_name = $_POST['school_name'];
 // $retval = mysqli_query( $link , $sql); 
 
 
+
+
 for($x = 0 ; $x<count($year) ; $x++)
 {
     for($i = 0 ; $i<count($school_name) ; $i++)
     {
-    $sql = "SELECT * FROM `$year[$x]` WHERE `school_name` = '$school_name[$i]'";
-    $retval = mysqli_query( $link , $sql); 
+        $sql = "SELECT * FROM `$year[$x]` WHERE `school_name` = '$school_name[$i]'";
+        $retval = mysqli_query( $link , $sql); 
 
-    // echo
-    //     $year[$i];
-    while($row = mysqli_fetch_array($retval)) {
-    
-    echo 
-       "學年度 : {$year[$x]}  <br> ".
-       "學校 :{$row['school_name']}  <br> ".
-       "醫學系 : {$row['medical_science']} <br> ".
-       "牙醫學系 : {$row['dentist']} <br> ".
-       "醫學院 : {$row['medical']} <br> ".
-       "工學院 : {$row['institute']} <br> ".
-       "理農學院 : {$row['College_of_Agriculture']} <br> ".
-       "商學院 : {$row['schools']} <br> ".
-       "文法學院 : {$row['Grammar_School']} <br> ".
-       "其他 : {$row['items']} <br> ".
-       "--------------------------------<br>";
-    
-    }
+        // echo
+        //     $retval;
+        while($row = mysqli_fetch_array($retval)) {
+        
+        echo 
+        "學年度 : {$year[$x]}  <br> ".
+        "學校 :{$row['school_name']}  <br> ".
+        "醫學系 : {$row['medical_science']} <br> ".
+        "牙醫學系 : {$row['dentist']} <br> ".
+        "醫學院 : {$row['medical']} <br> ".
+        "工學院 : {$row['institute']} <br> ".
+        "理農學院 : {$row['College_of_Agriculture']} <br> ".
+        "商學院 : {$row['schools']} <br> ".
+        "文法學院 : {$row['Grammar_School']} <br> ".
+        "其他 : {$row['items']} <br> ".
+        "--------------------------------<br>";
+        
+        }
 
     }
 }
