d19eb5e41e2bdfa4f3f8fc7f108c3aed815db9f4
cody0322 <z1510110@gmail.com>
2022-06-08 03:13:41 +0800

    search_OK

--- home/testserach.php
+++ home/testserach.php
@@ -17,34 +17,34 @@ $school_name = $_POST['school_name'];
 
 
 
-for($x = 0 ; $x<count($year) ; $x++)
-{
-    for($i = 0 ; $i<count($school_name) ; $i++)
-    {
-        $sql = "SELECT * FROM `$year[$x]` WHERE `school_name` = '$school_name[$i]'";
-        $retval = mysqli_query( $link , $sql); 
+// for($x = 0 ; $x<count($year) ; $x++)
+// {
+//     for($i = 0 ; $i<count($school_name) ; $i++)
+//     {
+//         $sql = "SELECT * FROM `$year[$x]` WHERE `school_name` = '$school_name[$i]'";
+//         $retval = mysqli_query( $link , $sql); 
 
-        // echo
-        //     $retval;
-        while($row = mysqli_fetch_array($retval)) {
+//         // echo
+//         //     $retval;
+//         while($row = mysqli_fetch_array($retval)) {
         
-        echo 
-        "學年度 : {$year[$x]}  <br> ".
-        "學校 :{$row['school_name']}  <br> ".
-        "醫學系 : {$row['medical_science']} <br> ".
-        "牙醫學系 : {$row['dentist']} <br> ".
-        "醫學院 : {$row['medical']} <br> ".
-        "工學院 : {$row['institute']} <br> ".
-        "理農學院 : {$row['College_of_Agriculture']} <br> ".
-        "商學院 : {$row['schools']} <br> ".
-        "文法學院 : {$row['Grammar_School']} <br> ".
-        "其他 : {$row['items']} <br> ".
-        "--------------------------------<br>";
+//         echo 
+//         "學年度 : {$year[$x]}  <br> ".
+//         "學校 :{$row['school_name']}  <br> ".
+//         "醫學系 : {$row['medical_science']} <br> ".
+//         "牙醫學系 : {$row['dentist']} <br> ".
+//         "醫學院 : {$row['medical']} <br> ".
+//         "工學院 : {$row['institute']} <br> ".
+//         "理農學院 : {$row['College_of_Agriculture']} <br> ".
+//         "商學院 : {$row['schools']} <br> ".
+//         "文法學院 : {$row['Grammar_School']} <br> ".
+//         "其他 : {$row['items']} <br> ".
+//         "--------------------------------<br>";
         
-        }
+//         }
 
-    }
-}
+//     }
+// }
 
 
 
@@ -67,3 +67,73 @@ for($x = 0 ; $x<count($year) ; $x++)
 //  }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>查詢結果</title>
+    
+    <button name="home" style="margin-top:10px"><a href="home.html">首頁</button></a>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0-beta1/css/bootstrap.min.css ">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta3/css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="search.css">
+</head>
+<body>
+    
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">學年度</th>
+                <th scope="col">學校名稱</th>
+                <th scope="col">醫學系</th>
+                <th scope="col">牙醫學系</th>
+                <th scope="col">醫學院</th>
+                <th scope="col">工學院</th>
+                <th scope="col">理農學院</th>
+                <th scope="col">文法學院</th>
+                </tr>
+            </thead>
+        <?php
+        for($x = 0 ; $x<count($year) ; $x++)
+        {
+            for($i = 0 ; $i<count($school_name) ; $i++)
+            {
+                $sql = "SELECT * FROM `$year[$x]` WHERE `school_name` = '$school_name[$i]'";
+                $retval = mysqli_query( $link , $sql); 
+        
+                // echo
+                //     $retval;
+                while($row = mysqli_fetch_array($retval)) {
+                
+                echo 
+                "<tbody>".
+                    "<tr>".
+                        "<td>".
+                        "{$year[$x]}  <br> ".
+                        "</td>".
+                        "<td>".
+                        "{$row['school_name']}  <br> ".
+                        "</td>".
+                    "</tr>".
+                "</tbody>";
+                
+                
+                
+                }
+        
+            }
+        }
+        
+        ?>
+</table>
+ 
+    </div>
+
+
+</body>
+</html>
